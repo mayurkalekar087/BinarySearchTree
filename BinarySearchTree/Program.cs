813cb0f06b600943bf4148120e741d2e510b01cc
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-17 11:40:43 +0530

    feat:Welcome Message

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree Demo !");
+        }
+    }
+}
