4aacb45ef2610f1c53eafcfd68b53a23bd926764
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-17 12:31:30 +0530

    feat:Ability to create BST

---
+++ BinarySearchTree/BST.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BST<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BST<T> LeftTree { get; set; }
+        public BST<T> RightTree { get; set; }
+        public BST(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if (( currentNodeValue.CompareTo(item) == 0))
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BST<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BST<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+        public bool IfExists(T element, BST<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("current element is {0} in BST", node.NodeData);
+            }
+            if (element.CompareTo(node.NodeData) < 0) ;
+            {
+                IfExists(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0) ;
+            {
+                IfExists(element, node.RightTree);
+            }
+            return result;
+        }
+    }
+}
+
+      
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,10 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Binary Search Tree Demo !");
+            BST<int> BinarySearch = new BST<int>(56);
+            BinarySearch.Insert(30);
+            BinarySearch.Insert(70);
+            BinarySearch.Display();
         }
     }
 }
